a712f18e80036bb4b99ec1ba25372078c07a88d1
MahadShbeeb <45308488+MahadShbeeb@users.noreply.github.com>
2020-04-29 01:04:15 +0300

    extract

--- server/NLP/MahadNLP.py
+++ server/NLP/MahadNLP.py
@@ -15,90 +15,75 @@ def main(text):
     data = re.findall(pattern, text)
 
     answers = []
-    if re.match(r'المطلوب', ''.join(data)):
-        problem = text.split(sep="المطلوب")
-        answers = problem[-1]
-    else:
-        for match in data:
-            if re.findall(r'a.|b.|c.|d.|a\)|b\)|c\)|d\)', match):
-                answers.append(match)
+    for match in data:
+        if re.findall(r'a\.|b\.|c\.|d\.|a\)|b\)|c\)|d\)', match):
+            answers.append(match)
 
-    unit_value = quantites = []
-    res = {}
+    unit_value = quantites = res = []
 
     data = [i for i in data if i not in answers]
     unit_value = [i for i in data if re.match(r'^[0-9]+', i)]
     quantites = [i for i in data if i not in unit_value]
-    finished_quantite = [re.match(r'\w+\=\d+(.*)?', i).group(0) for i in quantites if
-                         re.match(r'\w+\=\d+(.*)?', i) != None]
+    finished_quantite = [re.findall(r'\S+\=\S+', i)[0] for i in quantites if len(re.findall(r'\S+\=\S+', i)) != 0]
 
     if len(finished_quantite) >= 0:
         for i in finished_quantite:
             quantites.remove(i)
 
-    while len(quantites) > len(unit_value):
-        unit_value.append('?')
-    while len(unit_value) > len(quantites):
-        quantites.append('?')
+    [res.append((i, '?')) for i in unit_value]
+    [res.append(('?', i)) for i in quantites]
 
-    res = dict(zip(unit_value, quantites))
+    new_res = []
+    for quant_and_val in res:
+        if quant_and_val[0] == '?':
+            new_res.append((quant_and_val))
+        else:
+            for key, value in unit.data.items():
+                for i in value['for']:
+                    if re.sub(r'{}'.format(re.findall(r'^\d+\.\d+|^\d+', str(quant_and_val[0]))[0]), '',
+                              quant_and_val[0]) == key:
+                        new_res.append((quant_and_val[0], i['symbol']))
 
-    unknown_value = ''
-
-    for k, v in res.items():
-        for key, value in unit.data.items():
-            for i in value['for']:
-                if len(re.findall(r'[\D]+', str(k))) == 0 and re.findall(r'[\D]+', str(k))[0] != key and len(re.findall(r'[\D]+', str(v))) == 0 and re.findall(r'[\D]+', str(v))[0] != i['symbol'] and k == '?':
-                    res[k] = i['symbol']
-                    unknown_value = v
-
-    if len(finished_quantite) == 1:
-        finished_quantite = re.split(r'=', finished_quantite[0])
-        res[finished_quantite[1]] = finished_quantite[0]
-    elif len(finished_quantite) > 1:
+    if len(finished_quantite) > 1:
         for i in finished_quantite:
             i = re.split(r'=', i)
-            res[i[1]] = i[0]
-
-    if unknown_value != '?' and unknown_value != '':
-        res[unknown_value] = '?'
+            new_res.append((i[1], i[0]))
+    elif len(finished_quantite) == 1:
+        finished_quantite = re.split(r'=', finished_quantite[0])
+        new_res.append((finished_quantite[1], finished_quantite[0]))
 
     data_dicts = []
     quantity = Unit = sym = ''
     value = 0
 
-    for k, v in res.items():
-        if v != '?' and k == '?':
-            val = k
-            Unit = k
-            sym = v
+    for quant_and_val in new_res:
+        if quant_and_val[1] != '?' and quant_and_val[0] == '?':
+            val = quant_and_val[0]
+            Unit = quant_and_val[0]
+            sym = quant_and_val[1]
             for key, value in unit.data.items():
                 for i in value['for']:
-                    if re.findall(r'[\D]+', str(v))[0] == i['symbol']:
+                    if re.findall(r'[\D]+', str(quant_and_val[1]))[0] == i['symbol']:
                         quantity = i['name']
-        elif v != '?' and k != '?':
-            val = re.findall(r'^\d+', str(k))[0]
-            Unit = re.findall(r'\D+/.*|\D+\^\d|[\D]+', str(k))[0]
-            sym = v
-            for key, value in unit.data.items():
-                for i in value['for']:
-                    if Unit == key:
-                        quantity = i['name']
-        elif v == '?' and k != '?':
-            val = re.findall(r'^\d+', str(k))[0]
-            Unit = re.findall(r'\D+/.*|\D+\^\d|[\D]+', str(k))[0]
+
+        elif quant_and_val[1] == '?' and quant_and_val[0] != '?':
+            val = re.findall(r'^\d+\.\d+|^\d+', str(quant_and_val[0]))[0] if len(
+                re.findall(r'^\d+', str(quant_and_val[0]))) != 0 else '?'
+            Unit = re.sub(r'{}'.format(val), '', str(quant_and_val[0])) if val != '?' else '?'
             for key, value in unit.data.items():
                 for i in value['for']:
                     if Unit == key:
                         quantity = i['name']
                         sym = i['symbol']
         else:
-            val = v
-            Unit = v
-            sym = k
+
+            val = re.findall(r'^\d+\.\d+|^\d+', str(quant_and_val[0]))[0] if len(
+                re.findall(r'^\d+', str(quant_and_val[0]))) != 0 else '?'
+            Unit = re.sub(r'{}'.format(val), '', str(quant_and_val[0])) if val != '?' else '?'
+            sym = quant_and_val[1]
             for key, value in unit.data.items():
                 for i in value['for']:
-                    if k == i['symbol']:
+                    if quant_and_val[1] == i['symbol']:
                         quantity = i['name']
 
         dict1 = {'quantity': quantity, 'value': val, 'unit': Unit, 'symbol': sym}
